fe9a96f1410b3972c7a3e4f315731e2c449333bf
Andy Dingler <andy.dingler711@gmail.com>
2026-05-30 16:44:38 -0700

    fix(gateway): reapply resource pressure cleanup on upstream main

--- hermes_cli/gateway.py
+++ hermes_cli/gateway.py
@@ -2912,6 +2912,18 @@ def generate_launchd_plist() -> str:
         <key>SuccessfulExit</key>
         <false/>
     </dict>
+
+    <key>SoftResourceLimits</key>
+    <dict>
+        <key>NumberOfFiles</key>
+        <integer>4096</integer>
+    </dict>
+
+    <key>HardResourceLimits</key>
+    <dict>
+        <key>NumberOfFiles</key>
+        <integer>4096</integer>
+    </dict>
     
     <key>StandardOutPath</key>
     <string>{log_dir}/gateway.log</string>
